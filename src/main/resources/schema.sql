893a2ff86ed3f4c337003a73af20d90d45c6a06e
udr013 <udr013@gmail.com>
2016-06-03 15:58:09 +0200

    sql files updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,14 +15,25 @@ CREATE TABLE IF NOT EXISTS `trainer` (
   `id`         INT(11) NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(200)     DEFAULT NULL,
   `last_name`  VARCHAR(200)     DEFAULT NULL,
+  `username` VARCHAR(200)     DEFAULT NULL,
   `phone_nr`   VARCHAR(200)     DEFAULT NULL,
   `e_mail`     VARCHAR(200)     DEFAULT NULL,
-  `pass_word`  VARCHAR(200)     DEFAULT NULL,
+  `password`  VARCHAR(200)     DEFAULT NULL,
+  `enabled`     INT(1)     DEFAULT 1,
   PRIMARY KEY (`id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = latin1
   AUTO_INCREMENT = 6;
+
+  CREATE TABLE IF NOT EXISTS `user_roles` (
+  user_role_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  UNIQUE KEY uni_username_role (role,username),
+  KEY fk_username_idx (username)
+);
 
   CREATE TABLE IF NOT EXISTS `customer` (
   `id`         INT(11) NOT NULL AUTO_INCREMENT,
@@ -52,19 +63,26 @@ CREATE TABLE IF NOT EXISTS `subscription` (
   DEFAULT CHARSET = latin1
   AUTO_INCREMENT = 2;
 
-  CREATE TABLE IF NOT EXISTS `session` (
+CREATE TABLE IF NOT EXISTS `sport_session` (
   `id`              INT(11) NOT NULL AUTO_INCREMENT,
   `subscription_id` INT(11) NOT NULL,
-  `date`            VARCHAR(200)     DEFAULT NULL,
+  `customer_id` INT(11) NOT NULL,
+  `trainer_id` INT(11) NOT NULL,
+  `date_time`  DATETIME  DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `subscription_id` (`subscription_id`)
+  KEY `subscription_id` (`subscription_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `trainer_id` (`trainer_id`)
+
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = latin1
   AUTO_INCREMENT = 13;
 
-ALTER TABLE `session`
-  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`id`)
+ALTER TABLE `sport_session`
+  ADD CONSTRAINT `sport_session_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`id`)ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `sport_session_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `sport_session_ibfk_3` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
@@ -73,6 +91,7 @@ ALTER TABLE `subscription`
   ADD CONSTRAINT `subscription_ibfk_2` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
