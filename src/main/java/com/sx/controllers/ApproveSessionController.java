fc686d0461578c48eccd3e98bcd458d1307b934a
wsteenhuis81 <wsteenhuis@gmail.com>
2016-06-07 21:06:15 +0200

    - controller en view approveSession toegevoegd

---
+++ src/main/java/com/sx/controllers/ApproveSessionController.java
@@ -0,0 +1,44 @@
+package com.sx.controllers;
+
+import com.sx.models.SportSession;
+import com.sx.service.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Controller
+public class ApproveSessionController {
+
+    @Autowired
+    private SessionService sessionService;
+
+    SportSession thisSportSession;
+    String wrongPin = "";
+
+    @RequestMapping(value = "approvesession", method = RequestMethod.POST)
+    public String approveSession(SportSession sportSession, Model model){
+        thisSportSession = sportSession;
+        model.addAttribute("sportSession", sportSession);
+        return "/approve_session";
+    }
+    @RequestMapping(value = "customerapprove", method = RequestMethod.POST)
+    public String customerApprove(@RequestParam ("pin") String pin, Model model){
+        model.addAttribute("sportSession", thisSportSession);
+        if (Integer.parseInt(pin)==Integer.parseInt(thisSportSession.getCustomer().getPin())){
+            thisSportSession.setApproved(true);
+            thisSportSession.setTimeApproved(new Date());
+            sessionService.save(thisSportSession);
+            return "/list_scheduled_sessions";
+        }
+        String wrongPin = "Foutieve Pincode, probeer opnieuw";
+        model.addAttribute("wrongPin", wrongPin);
+        return "/approve_session";
+    }
+
+
+}
