bf0ecf0048f24a49c095fd2df6677fd74c3cab93
Sjoerd <sjoerd.de.jong1357@gmail.com>
2016-06-17 15:30:10 +0200

    Logout button op Admin home en Trainer home page

--- src/main/java/com/sx/controllers/IndexController.java
+++ src/main/java/com/sx/controllers/IndexController.java
@@ -13,10 +13,16 @@ import com.sx.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 @Controller
@@ -44,6 +50,14 @@ public class IndexController {
         return "/index";
     }
 
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
+    }
 
     @RequestMapping("/unauthorized")
     public String unauthorized(){
